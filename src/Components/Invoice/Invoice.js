14964edee951d83b74fb29ec8de82ee269767a07
Butch-beep <95406326+Butch-beep@users.noreply.github.com>
2023-05-05 17:36:24 +0800

    done individual invoice functionality

--- src/Components/Invoice/Invoice.js
+++ src/Components/Invoice/Invoice.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router'
 import { useQuery } from 'react-query'
 import axios from 'axios'
@@ -9,106 +9,99 @@ function Invoice() {
     const params = useParams()
     const invoiceId = params.invoiceId
 
-    let promise = new Promise(function(resolve, reject) {
-        let data = fetch('http://localhost:4000/invoices')
-    
-        if (data) {
-            resolve(data)
+
+    const { isLoading, data } = useQuery('invoice', () => {
+        return axios.get('http://localhost:4000/invoices')
+    }) 
+
+    const index = data?.data.findIndex(invoice => invoice.id === invoiceId)
+
+
+    const toDate = (date) =>  {
+        if (date) {
+            const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+            const dateSplit = date.split('-')
+            const monthNum = removeZero(dateSplit[1])
+            return `${dateSplit[2]} ${month[monthNum]} ${dateSplit[0]}`
         }
-    })
+    }
 
-    promise.then(response => response.json()).then(data => console.log(data))
+    const removeZero = (num) => {
+        const arr = num.split('')
+        return arr[1]
+    }
 
+    const upperCaseFirstLetter = (string) => {
+        if (string) {
+            return string.charAt(0).toUpperCase() + string.slice(1)
+        }
+    }
 
-    // const { isLoading, data } = useQuery('invoice', () => {
-    //     return axios.get('http://localhost:4000/invoices')
-    // }) 
+    const getStatus = (status) => {
+        if (data) {
+            switch (status) {
+                case 'paid':
+                    return 'green'
+                    break;
+                case 'pending':
+                    return 'orange'
+                    break;
+                case 'draft':
+                    return 'white'
+                    break;
+            }
+        }
+    } 
 
-    // const invoice = (param) => {
-    //     if (data) {
-    //         const index = invoiceIndex();
-    //         return data.data[index][`${param}`];
-    //     }
-    // }
-
-    // const invoiceIndex = () => {
-    //     if (data) {
-    //         const fetchResult = data.data
-    //         const index = fetchResult.findIndex(invoice => invoice.id === invoiceId)
-    //         return index
-    //     }
-    // }
-
-    // let promise  = new Promise(function(resolve, reject) {
-    //     let data = fetch('http://localhost:4000/invoices')
-    //     if (data !== undefined) {
-    //         return resolve(data)
-    //     }
-    // })
-
-
-    // const isInvoiceIdExist = () => {
-    //     if (data.statusText) {
-    //         const arrInvoice = data.data.map((invoice) => invoice.id)
-    //         if (arrInvoice.includes(invoiceId)) {
-    //             console.log('hey')
-    //             return data.data[1].id
-    //         }
-    //     }
-    // }
-
-
-
-
-    // const invoiceIndex = () => {
-    //     return isInvoiceIdExist().findIndex((invoice) => invoice.id === invoiceId)
-    // } 
-
-    // console.log(invoiceIndex())
-
-    // const invoice = () => {
-    //     const invoiceIndex = () => {
-    //         return isInvoiceIdExist().findIndex((invoice) => invoice.id === invoiceId)
-    //     } 
-
-    //     console.log(isInvoiceIdExist()[invoiceIndex])
-    // } 
-
-    // console.log(invoice())
-
-    // if (isInvoiceIdExist() === false)  {
-    //     return <div>No Invoice with Invoice ID of {invoiceId} was found</div>
-    // }
 
   return (
     <div className='invoice'>
         <div className='invoice__container invoice__container--header'>
             <div className='invoice__container invoice__container--status'>
                 <p className='invoice__text invoice__text--status'>Status</p>
-                <span className='invoice__symbol'>Icon</span>
-                <p className='invoice__text'>Pending</p>
+                <span className={`invoice__symbol--${getStatus(data?.data[index].status)}`}>Icon</span>
+                <p className='invoice__text'>{upperCaseFirstLetter(data?.data[index].status)}</p>
             </div>
             <div className='invoice__container invoice__container--updator'>
-                <p className='invoice__updator invoice__updator--edit'>Edit</p>
-                <p className='invoice__updator invoice__updator--delete'>Delete</p>
-                <p className='invoice__updator invoice__updator-paid'>Mark as Paid</p>
+                <button className='invoice__updator invoice__updator--edit' type='button'>Edit</button>
+                <button className='invoice__updator invoice__updator--delete' type='button'>Delete</button>
+                <button className={`invoice__updator invoice__updator--paid${data?.data[index].status === 'paid' ? '--invisible' : ''}`} type='button'>Mark as Paid</button>
             </div>
         </div>
         <div className='invoice__container invoice__container--body'>
             <div className='invoice__container invoice__container--bill-from'>
-                <p className='invoice__text invoice__text--from-id-number'></p>
-                <p className='invoice__text invoice__text--from-address'>Bill From Address</p>
+                <div className='invoice__container container--id-description'>
+                    <p className='invoice__text invoice__text--from-id-number'>{data?.data[index].id}</p>
+                    <p className='invoice__text invoice__text--from-description'>{data?.data[index].description}</p>
+                </div>
+                <div className='invoice__container container--from-address'>
+                    <p className='invoice--address address--from-street'>{data?.data[index].senderAddress.street}</p>
+                    <p className='invoice--address address--from-city'>{data?.data[index].senderAddress.city}</p>
+                    <p className='invoice--address address--from-post-code'>{data?.data[index].senderAddress.postCode}</p>
+                    <p className='invoice--address address--from-country'>{data?.data[index].senderAddress.country}</p>
+                </div>
             </div>
             <div className='invoice__container invoice__container--bill-to'>
                 <div className='invoice__container invoice__container--dates'>
                     <p className='invoice__text invoice__text--invoice-date'>Invoice Date</p>
-                    <p className='invoice__text invoice__text--payment-due'>Payment Due</p>
+                    <p className='invoice__text invoice__text--invoice-date-value'>{toDate(data?.data[index].createdAt)}</p>
+                    <p className='invoice__text invoice__text--payment-due'>Payment Date</p>
+                    <p className='invoice__text invoice__text--invoice-date-value'>{toDate(data?.data[index].createdAt)}</p>
                 </div>
                 <div className='invoice__container invoice__container--name-address'>
-                    <p className='invoice__text invoice__text--to-name'>Bill To Name</p>
-                    <p className='invoice__text invoice__text--to-address'>Bill To Address</p>
+                    <p className='invoice__text invoice__text--bill-to'>Bill To</p>
+                    <p className='invoice__text invoice__text--to-name'>{data?.data[index].clientName}</p>
+                    <div className='invoice__container container--from-address'>
+                        <p className='invoice--address address--from-street'>{data?.data[index].clientAddress.street}</p>
+                        <p className='invoice--address address--from-city'>{data?.data[index].clientAddress.city}</p>
+                        <p className='invoice--address address--from-post-code'>{data?.data[index].clientAddress.postCode}</p>
+                        <p className='invoice--address address--from-country'>{data?.data[index].clientAddress.country}</p>
+                    </div>
                 </div>
-                <p className='invoice__text invoice__text--sent-to'>Sent To Email</p>
+                <div className='invoice__container container--sent-to'>
+                    <p className='invoice__text invoice__text--client-email'>Client Email</p>
+                    <p className='invoice__text invoice__text--sent-to'>{data?.data[index].clientEmail}</p>
+                </div>
             </div>
             <div className='invoice__container invoice__container--invoice-summary'>
                 <div className='invoice__container invoice__container--computation'>
@@ -119,16 +112,16 @@ function Invoice() {
                         <p className='invoice__text invoice__text--total'>Total</p>
                     </div>
                     <div className='invoice__container--particular-value'>
-                        <p className='invoice__text invoice__text--item-name-value'>Brand Guidelines</p>
-                        <p className='invoice__text invoice__text--quantity-value'>1</p>
-                        <p className='invoice__text invoice__text--price-value'>1,800.90</p>
-                        <p className='invoice__text invoice__text--total-value'>1,800.90</p>
+                        <p className='invoice__text invoice__text--item-name-value'>{data?.data[index].items[0].name}</p>
+                        <p className='invoice__text invoice__text--quantity-value'>{data?.data[index].items[0].quantity}</p>
+                        <p className='invoice__text invoice__text--price-value'>{data?.data[index].items[0].price}</p>
+                        <p className='invoice__text invoice__text--total-value'>{data?.data[index].items[0].total}</p>
                     </div>
 
                 </div>
                 <div className='invoice__container--grand-total'>
                     <p className='invoice__text invoice__text--grand-total'>Amount Due</p>
-                    <p className='invoice__number invoice__number--grant-total'>Grand Total Number</p>
+                    <p className='invoice__number invoice__number--grant-total'>{data?.data[index].total}</p>
                 </div>
             </div>
         </div>
